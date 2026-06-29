c42b6db9d11d76471b24baf32b1c753f72f9ceff
GabrielaDias01 <gabrieladias2410@gmail.com>
2026-06-29 19:51:28 -0300

    Arrumando a função de sair do perfil

--- FRONT/CONTROLLER_VIEW/perfil.js
+++ FRONT/CONTROLLER_VIEW/perfil.js
@@ -2,29 +2,25 @@ const token = localStorage.getItem("token");
 const usuario = JSON.parse(localStorage.getItem("usuario"));
 
 if (!token || !usuario) {
-    window.location.href = "../../login.html";
+    window.location.href = "login.html";
 }
 
 document.querySelector("#nome").textContent = usuario.nome;
 document.querySelector("#email").textContent = usuario.email;
 document.querySelector("#perfil").textContent = usuario.perfil;
 
+const usuarioArea = document.querySelector("#usuarioArea");
 const areaAdm = document.querySelector("#admArea");
 
 if (usuario.perfil !== "administrador") {
-
     areaAdm.style.display = "none";
-
 }
 
-if (usuario.perfil === "administrador"){
+if (usuario.perfil === "administrador") {
     usuarioArea.style.display = "none";
 }
 
 document.querySelector("#btnSair").addEventListener("click", () => {
-
     localStorage.clear();
-
-    window.location.href = "login.html";
-
+    window.location.href = "../../login.html";
 });
